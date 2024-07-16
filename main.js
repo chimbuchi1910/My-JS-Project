295cc14fcd1bb6d67ecf91453383a76f1c04489a
Chimbuchi <echimbuchi33@gmail.com>
2024-07-16 05:41:58 -0700

    my JS Project

--- main.js
+++ main.js
@@ -25,18 +25,139 @@
 // student.push("gate", "hell");
 // console.log(student);
 
-const person = {
-  FirstName: "Henry",
-  LastName: "Peter",
-  Score: 40.5,
-  isAdmin: false,
-  Hobbies: ["cooking", "bet9ja", "boxing"],
-  Address: {
-    City: "Owerri",
-    LGA: "Owerri-West",
-    PostalCode: 460120,
-  },
-};
-const { FirstName, LastName } = person;
-console.log(`${FirstName} ${LastName}`);
+// const person = {
+//   FirstName: "Henry",
+//   LastName: "Peter",
+//   Score: 40.5,
+//   isAdmin: false,
+//   Hobbies: ["cooking", "bet9ja", "boxing"],
+//   Address: {
+//     City: "Owerri",
+//     LGA: "Owerri-West",
+//     PostalCode: 460120,
+//   },
+// };
+// console.log(person.Address.LGA);
+// const { FirstName, LastName } = person;
+// console.log(`${FirstName} ${LastName}`);
 // console.log(JSON.stringify(person));     JSON format
+
+// const cartItems = [
+//   {
+//     id: 1,
+//     productName: "Sugar",
+//     quantity: 5,
+//     isPaid: false,
+//     category: "beverages",
+//     price: 1000,
+//   },
+//   {
+//     id: 2,
+//     productName: "Fridge",
+//     quantity: 2,
+//     isPaid: false,
+//     category: "electronics",
+//     price: 1000,
+//   },
+//   {
+//     id: 3,
+//     productName: "Milk",
+//     quantity: 5,
+//     isPaid: true,
+//     category: "beverages",
+//     price: 1000,
+//   },
+//   {
+//     id: 4,
+//     productName: "Milo",
+//     quantity: 5,
+//     isPaid: false,
+//     category: "beverages",
+//     price: 2000,
+//   },
+// ];
+// console.log(cartItems[0].id);
+
+// for loops
+// for (let i = 0; i <= 20; i++) {
+//   console.log(i);
+// }
+// let i = 0;
+// while (i <= 25) {
+//   console.log(`This is the loop of ${i}`);
+//   i++;
+// }
+
+const cartItems = [
+  {
+    id: 1,
+    productName: "Sugar",
+    quantity: 5,
+    isPaid: false,
+    category: "beverages",
+    price: 1000,
+  },
+  {
+    id: 2,
+    productName: "Fridge",
+    quantity: 2,
+    isPaid: false,
+    category: "electronics",
+    price: 1000,
+  },
+  {
+    id: 3,
+    productName: "Milk",
+    quantity: 5,
+    isPaid: true,
+    category: "beverages",
+    price: 1000,
+  },
+  {
+    id: 4,
+    productName: "Milo",
+    quantity: 5,
+    isPaid: false,
+    category: "beverages",
+    price: 2000,
+  },
+];
+const store = cartItems
+  .filter(function (listItems) {
+    return listItems.price === 1000;
+  })
+  .map(function (listItems) {
+    return listItems.productName;
+  });
+console.log(store);
+
+// (filter)
+// const store = cartItems.filter(function (listItems) {
+//   return listItems.isPaid === false;
+// });
+// console.log(store);
+
+// (map loop)
+// const store = cartItems.map(function (listItems) {
+//   return listItems.productname;
+// });
+// console.log(store);
+
+// (forEach)
+// cartItems.forEach(function (items) {
+//   console.log(items.productName);
+// });
+
+// for (items of cartItems) {
+//   console.log(items.productName);
+// }
+
+// let i = 0;
+// while (i <= cartItems.length) {
+//   console.log(cartItems[i].category);
+//   i++;
+// }
+
+// for (let i = 0; i <= cartItems.length; i++) {
+//   console.log(cartItems[i].productName);
+// }
